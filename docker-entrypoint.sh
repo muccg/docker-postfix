35e99f9f4740d406d6e20a7219c5114f7ccf8a9a
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2015-02-19 16:21:31 +0800

    cleanups

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,19 +7,12 @@ echo "WHOAMI is `whoami`"
 set -e
 
 qdir=/data/queue
-mkdir -p /data/queue
-[ -f /data/queue/etc/hosts ] && cp -a /etc/hosts /data/queue/etc/hosts
-[ -f /data/queue/etc/services ] && cp -f /etc/services /data/queue/etc/services
-
 mkdir -p "$qdir"/etc
 FILES="etc/localtime etc/services etc/resolv.conf etc/hosts \
     etc/host.conf etc/nsswitch.conf"
 for file in $FILES; do
     cp -a /"$file" "$qdir"/"$file"
 done
-
-
-sleep 5
 
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 
